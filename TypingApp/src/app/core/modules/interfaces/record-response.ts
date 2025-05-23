8dc290b2b6dfc7050f861185736792557ecc542b
Di <b.dima1916@gmail.com>
2025-05-23 23:54:09 +0300

    main proj

---
+++ TypingApp/src/app/core/modules/interfaces/record-response.ts
@@ -0,0 +1,10 @@
+export interface RecordResponse {
+    userId: string;
+    wpm:number;
+    raw:number;
+    accuracy: number;
+    consistency: number;
+    chars: number;
+    matchTime: number;
+    dateRecord: string; 
+}
